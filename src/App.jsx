b9624d55cef460de5a741f32f069d32c4835de49
Matthew Bolan <Bolan.Matthew.MB@Gmail.com>
2021-11-02 13:44:22 -0500

    Update

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+import Pokemon from './components/Pokemon';
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <h1>Gotta Catch'em All!</h1>
+
+      <Pokemon></Pokemon>
+
+    </div>
+
+  );
+
+}
+
+export default App;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+
+import axios from 'axios';
+
+const Pokemon = () => {
+
+    const [allPokemon, setAllPokemon] = useState([])
+    
+    const getPokemonInfo = ()=>{
+
+        axios.get('https://pokeapi.co/api/v2/pokemon?offset=807&limit=807')
+
+            .then(response =>{
+
+                setAllPokemon(response.data.results)
+
+            })
+
+    }
+
+    return (
+
+        <div>
+
+            <h3>Here are all the Pokemon!</h3>
+
+            <button onClick = {getPokemonInfo}>Fetch Pokemon</button>
+            {
+                allPokemon.map((poke,x)=>{
+                    
+                    return (
+
+                        <div key = {x}>
+
+                            
+
+                            <li>{poke.name}</li>
+
+                            
+
+                        </div>
+
+                    )
+
+                })
+
+            }
+
+        </div>
+
+    );
+
+};
+
+export default Pokemon;
